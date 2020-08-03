71f12f10f62bbcaa0f67c7e1aa1f9e14612c45b7
waseeme <wmohammed@leaders6-12.org>
2020-08-03 18:00:32 -0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      motorcycles: [],
+    };
+  }
+  async componentDidMount() {
+    const data = await axios("https://api.airtable.com/v0/appBcQntEGzgoK1Ad/favorites?api_key=keyzDeexKzGh6V5pz")
+      
+    this.setState({
+      motorcycles: data.data.records
+    })
+    
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              {this.state.motorcycles.map((motorcycle) => (
+                <MotorCycleCard {...motorcycle.fields} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const MotorCycleCard = ({
+  make,
+  model,
+  year,
+  description,
+  imageURL,
+  features,
+}) => (
+  <div className="card">
+    <img
+      className="card-img-top"
+      src={imageURL[0].url}
+      alt="Motorcycle poster"
+    />
+    <div className="card-body">
+      <h5 className="card-title">{make}</h5>
+      <h5 className="card-title">{model}</h5>
+      <p className="card-text">{description}</p>
+      <p className="card-text">{features}</p>
+      <small className="text-muted">{year}</small>
+    </div>
+  </div>
+);
