3e6e0e96fb241b84ec4774ad923fa6bd5d82ff26
waseeme <wmohammed@leaders6-12.org>
2021-06-16 16:30:15 -0400

    datadog-test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ import "./App.css";
 import Nav from "./Nav";
 import track from "./track.jpg";
 
+import { DdSdkReactNative, DdSdkReactNativeConfiguration } from 'dd-sdk-reactnative';
+
+const config = new DdSdkReactNativeConfiguration(
+    "pubcd2a5324e8caa431c5cfe5190ed50c5d", 
+    "<ENVIRONMENT_NAME>", 
+    "ef070bfe-86b5-4df4-a3a2-25eb4201510a",
+    true, // track User interactions (e.g.: Tap on buttons)
+    true, // track XHR Resources
+    true // track Errors
+)
+
+DdSdkReactNative.initialize(config)
+
+// const tracer = require('dd-trace').init()
+
 function App() {
   return (
     <>
